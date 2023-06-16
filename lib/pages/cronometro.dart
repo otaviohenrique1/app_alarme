2083aab83eacb8eff2d67204893ea497b1aeed3d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-16 17:45:08 -0300

    commit 8

--- lib/pages/cronometro.dart
+++ lib/pages/cronometro.dart
@@ -1,24 +1,154 @@
-import 'package:app_alarme/components/botao_flutuante.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Cronometro extends StatelessWidget {
+class Cronometro extends StatefulWidget {
   const Cronometro({
     super.key,
   });
 
   @override
+  State<Cronometro> createState() => _CronometroState();
+}
+
+class _CronometroState extends State<Cronometro> {
+  int _minutos = 0;
+  int _segundos = 0;
+  int _milissegundos = 0;
+  Timer? _timer;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // Inicia o temporizador
+  //   _startTimer();
+  // }
+
+  // @override
+  // void dispose() {
+  //   // Cancela o temporizador ao sair da tela
+  //   _cancelTimer();
+  //   super.dispose();
+  // }
+
+  void _startTimer() {
+    // Cria um temporizador que executa a função a cada 1 segundo
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _milissegundos += 10;
+        if (_milissegundos >= 1000) {
+          _milissegundos = 0;
+          _segundos++;
+          if (_segundos >= 60) {
+            _segundos = 0;
+            _minutos++;
+          }
+        }
+      });
+    });
+  }
+
+  void _cancelTimer() {
+    // Cancela o temporizador se estiver ativo
+    if (_timer != null) {
+      _timer!.cancel();
+      _timer = null;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
+    return ListView(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(16),
       children: [
-        ListView(
-          shrinkWrap: true,
-          children: [],
+        // _minutos
+        // _segundos
+        // _milissegundos
+        CronometroPequeno(
+          minutos: _minutos,
+          segundos: _segundos,
+          milissegundos: _milissegundos,
         ),
-        Positioned(
-          bottom: 16,
-          right: 16,
-          child: BotaoFlutuante(onPressed: () {}),
+        CronometroGrande(
+          minutos: _minutos,
+          segundos: _segundos,
+          milissegundos: _milissegundos,
         ),
+        ElevatedButton(
+          onPressed: _startTimer,
+          child: const Text("Iniciar"),
+        ),
+        ElevatedButton(
+          onPressed: _cancelTimer,
+          child: const Text("Parar"),
+        ),
+      ],
+    );
+  }
+}
+
+class CronometroGrande extends StatelessWidget {
+  const CronometroGrande({
+    super.key,
+    required int minutos,
+    required int segundos,
+    required int milissegundos,
+  })  : _minutos = minutos,
+        _segundos = segundos,
+        _milissegundos = milissegundos;
+
+  final int _minutos;
+  final int _segundos;
+  final int _milissegundos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(_minutos.toString(),
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 72)),
+        const Text(":",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 72)),
+        Text(_segundos.toString(),
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 72)),
+        const Text(":",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 72)),
+        Text(_milissegundos.toString(),
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 72)),
+      ],
+    );
+  }
+}
+
+class CronometroPequeno extends StatelessWidget {
+  const CronometroPequeno({
+    super.key,
+    required int minutos,
+    required int segundos,
+    required int milissegundos,
+  })  : _minutos = minutos,
+        _segundos = segundos,
+        _milissegundos = milissegundos;
+
+  final int _minutos;
+  final int _segundos;
+  final int _milissegundos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text("$_minutos",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+        const Text(":",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+        Text("$_segundos",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+        const Text(":",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+        Text("$_milissegundos",
+            textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
       ],
     );
   }
--- lib/pages/novo_alarme.dart
+++ lib/pages/novo_alarme.dart
@@ -8,19 +8,45 @@ class NovoAlarme extends StatefulWidget {
 }
 
 class _NovoAlarmeState extends State<NovoAlarme> {
-  bool repetir = false;
+  final TextEditingController _etiquetaController = TextEditingController();
   bool pular = false;
   bool tomChamada = false;
+  String texto = "---";
+
+  TimeOfDay? _selectedTime;
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+
+    if (pickedTime != null) {
+      setState(() {
+        _selectedTime = pickedTime;
+      });
+    }
+  }
+
+  String hora = "";
+  String minuto = "";
 
   @override
   Widget build(BuildContext context) {
+    hora = _selectedTime != null ? "${_selectedTime!.hour}" : '00';
+    minuto = _selectedTime != null ? "${_selectedTime!.minute}" : '00';
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: const Text("Configurar alarme"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                texto = _etiquetaController.text;
+              });
+            },
             icon: const Icon(Icons.check),
           ),
         ],
@@ -28,100 +54,159 @@ class _NovoAlarmeState extends State<NovoAlarme> {
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                ),
-              ),
-              const SizedBox(width: 8),
-              Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                ),
-              ),
-            ],
+          _Relogio(hora: hora, minuto: minuto),
+          ElevatedButton(
+            onPressed: () => _selectTime(context),
+            child: Text('Selecionar hora'),
           ),
           const Divider(),
-          const TextField(
-            decoration: InputDecoration(
-              hintText: "Etiqueta",
-              border: InputBorder.none,
-            ),
-          ),
-          const Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text("Repetir"),
-              Switch(
-                value: repetir,
-                onChanged: (value) {
-                  setState(() {
-                    repetir = value;
-                  });
-                },
-              ),
-            ],
-          ),
-          const Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Pular"),
-                  Text("Pular para o proximo alarme"),
-                ],
-              ),
-              Switch(
-                value: pular,
-                onChanged: (value) {
-                  setState(() {
-                    pular = value;
-                  });
-                },
-              ),
-            ],
-          ),
-          const Divider(),
-          InkWell(
-            onTap: () {},
-            child: Ink(
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Tom de Chamada"),
-                  Text("AlarmBeep"),
-                ],
+          SizedBox(
+            height: 50,
+            child: TextField(
+              controller: _etiquetaController,
+              decoration: const InputDecoration(
+                hintText: "Etiqueta",
+                border: InputBorder.none,
               ),
             ),
           ),
           const Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text("Pular"),
-              Switch(
-                value: tomChamada,
-                onChanged: (value) {
-                  setState(() {
-                    tomChamada = value;
-                  });
-                },
+          SizedBox(
+            height: 50,
+            child: InkWell(
+              onTap: () {},
+              child: Ink(
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Repetir"),
+                    Text("Todo dia"),
+                  ],
+                ),
               ),
-            ],
+            ),
+          ),
+          const Divider(),
+          SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Pular"),
+                    Text("Pular para o proximo alarme"),
+                  ],
+                ),
+                Switch(
+                  value: pular,
+                  onChanged: (value) {
+                    setState(() {
+                      pular = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          const Divider(),
+          SizedBox(
+            height: 50,
+            child: InkWell(
+              onTap: () {},
+              child: Ink(
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Tom de Chamada"),
+                    Text("AlarmBeep"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Divider(),
+          SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Pular"),
+                Switch(
+                  value: tomChamada,
+                  onChanged: (value) {
+                    setState(() {
+                      tomChamada = value;
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
           const Divider(),
         ],
       ),
+    );
+  }
+}
+
+class _Relogio extends StatelessWidget {
+  const _Relogio({
+    // ignore: unused_element
+    super.key,
+    required this.hora,
+    required this.minuto,
+  });
+
+  final String hora;
+  final String minuto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _QuadradoComBorda(texto: hora),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Text(":", style: TextStyle(fontSize: 30)),
+          ),
+          _QuadradoComBorda(texto: minuto),
+        ],
+      ),
+    );
+  }
+}
+
+class _QuadradoComBorda extends StatelessWidget {
+  const _QuadradoComBorda({
+    // ignore: unused_element
+    super.key,
+    required this.texto,
+  });
+
+  final String texto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+      ),
+      child: Center(
+          child: Text(
+        texto,
+        style: const TextStyle(fontSize: 30),
+      )),
     );
   }
 }
